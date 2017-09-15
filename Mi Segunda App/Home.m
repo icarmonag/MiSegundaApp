456725e494bc3407def7743698e9b46b4751e1a3
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-09-15 08:53:30 -0500

    Tarea 2

--- Mi Segunda App/Home.m
+++ Mi Segunda App/Home.m
@@ -14,14 +14,26 @@
 
 @implementation Home
 
+NSArray *etiquetas;
+NSArray *colores;
+
+int i;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    etiquetas = [NSArray arrayWithObjects: @"Preparando...", @"Mezclando las piezas", @"Jugando con ellas", @"Armando piezas", @"Reto completado", nil];
+    colores = [NSArray arrayWithObjects: [UIColor blackColor],[UIColor greenColor],[UIColor blueColor],[UIColor orangeColor],[UIColor redColor],nil];
+    i=0;
 }
 
 - (IBAction)changeButtonPressed:(id)sender {
-    self.lblWelcome.text = @"Mentira!";
-
+    self.lblWelcome.text = [etiquetas objectAtIndex : i ];
+    self.lblWelcome.textColor = [colores objectAtIndex : i];
+    i=i+1;
+    if (i>4){
+        i=0;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
